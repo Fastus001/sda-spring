7ad32145d4c6a3b45d099bef1ea75fc98937e589
Fastus001 <tkarczmarzyk@gmail.com>
2021-04-18 10:55:41 +0200

    Profiles in application properties and in components.

--- src/main/java/pl/fastus/sdaspring/rest/car/bootstrap/CarDataInit.java
+++ src/main/java/pl/fastus/sdaspring/rest/car/bootstrap/CarDataInit.java
@@ -1,6 +1,8 @@
 package pl.fastus.sdaspring.rest.car.bootstrap;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import pl.fastus.sdaspring.rest.car.domain.Car;
 import pl.fastus.sdaspring.rest.car.services.CarService;
@@ -14,6 +16,8 @@ import java.util.List;
  */
 @RequiredArgsConstructor
 @Component
+@Profile("dev")
+@Slf4j
 public class CarDataInit {
 
     private final CarService carService;
@@ -21,18 +25,20 @@ public class CarDataInit {
     @PostConstruct
     public void addCars(){
         final List<Car> carsList = createCarsList();
+        carService.addCars(carsList);
+        log.info("sample data initialized!");
+    }
+
+    private List<Car> createCarsList(){
+        List<Car> cars = new ArrayList<>();
+
         final Car car = new Car();
         car.setModel("Toyota");
         car.setMake("Corolla");
         car.setYear(2000);
         car.setMileage(125000);
-        carsList.add(car);
+        cars.add(car);
 
-        carService.addCars(carsList);
-    }
-
-    private List<Car> createCarsList(){
-        List<Car> cars = new ArrayList<>();
         return cars;
     }
 }
